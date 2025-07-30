e7c7021bb10b13306daecd5dde78192a61e8df1a
markinatlanta <markjsandefur@gmail.com>
2025-07-30 15:57:01 -0400

    Update jks-faq-widget.js

--- widget/jks-faq-widget.js
+++ widget/jks-faq-widget.js
@@ -1,6 +1,6 @@
 <!-- Include this in your <head> or before </body> -->
-<script src="https://cdn.jsdelivr.net/gh/advisoryjks/jks-faq-widget@latest/jks-faq-widget.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/advisoryjks/jks-faq-widget@latest/jks-faq-widget.css" />
+<script src="https://cdn.jsdelivr.net/gh/advisoryjks/jks-faq-widget@main/jks-faq-widget.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/advisoryjks/jks-faq-widget@main/jks-faq-widget.css" />
 
 <script>
   window.JKS_FAQ_Widget.init({
